f5f832be668633243f186cc9c4b37253424e3680
Patrycja Grzenkowicz <patrycja.grzenkowicz2@teb.edu.pl>
2024-10-28 01:16:45 +0100

    Dodaj pliki projektów.

---
+++ Niewypal/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Niewypal
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
